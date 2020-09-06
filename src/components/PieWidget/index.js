8a4a4c68881e3afe2c9bf0e05c754b2e83fc1126
Luke Moderwell <luke.moderwell@gmail.com>
2020-09-06 14:51:37 -0400

    shrink percentages

--- src/components/PieWidget/index.js
+++ src/components/PieWidget/index.js
@@ -4,12 +4,17 @@ import Widget from '../Widget';
 import styles from './PieWidget.module.scss';
 
 export default function PieWidget({ data }) {
+  const defaultLabelStyle = {
+    fontSize: '5px',
+    fontFamily: 'sans-serif',
+  };
   return (
     <Widget title="Views By Platform">
       <PieChart
         data={data}
         radius={PieChart.defaultProps.radius - 8}
         label={({ dataEntry }) => Math.round(dataEntry.percentage) + '%'}
+        labelStyle={defaultLabelStyle}
         style={{ maxWidth: '80%', maxHeight: '80%', marginLeft: '10%' }}
       />
     </Widget>
